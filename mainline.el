eb3a345a384035c5669468665db76d75c2efda16
Jasonm23 <jasonm23@gmail.com>
2013-03-13 12:14:39 +1100

    updates

--- mainline.el
+++ mainline.el
@@ -18,8 +18,9 @@
 ;;; Commentary:
 ;;; 
 ;;; This is a fork of powerline.el which I began while the original
-;;; authorship of powerline was unknown, it turns out this was posted
-;;; by Nicolas Rougier posted to Emacswiki
+;;; authorship of powerline was unknown,
+;;;
+;;; (powerline.el 1.0.0 was posted to Emacswiki by Nicolas Rougier)
 ;;;
 ;;; Using mainline.el.
 ;;;
@@ -32,21 +33,31 @@
 ;;;
 ;;;     mainline-arrow-shape
 ;;;
-;;; possible values:
+;;; possible values...
 ;;;
 ;;;     chamfer
+;;;
 ;;;     chamfer14 (default)
+;;;
 ;;;     rounded
+;;;
 ;;;     arrow
+;;;
 ;;;     arrow-14
+;;;
 ;;;     slant
+;;;
 ;;;     slant-left
+;;;
 ;;;     slant-right;
+;;;
 ;;;     half
+;;;
 ;;;     percent
+;;;
 ;;;     curve
 ;;;
-;;; For screenshots and additional info see the article at:
+;;; For screenshots and additional info see the article at
 ;;; http://emacsfodder.github.com/blog/powerline-enhanced/
 ;;;
 
